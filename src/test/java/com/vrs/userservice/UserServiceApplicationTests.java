ab10ba5d097f6a19d1bb5bf0f2af6ae9a1ecc984
Vishnu5058 <vishnukantsalunke01@gmail.com>
2024-07-11 18:48:21 +0530

    created user service

---
+++ src/test/java/com/vrs/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrs.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
